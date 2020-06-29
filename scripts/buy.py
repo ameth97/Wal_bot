8e7360763f00d5892d1a04acfa0c7717553bb5db
Papa Ibrahima Sow <sowpi@ept.sn>
2020-06-29 21:53:40 +0000

    conf

--- scripts/buy.py
+++ scripts/buy.py
@@ -8,14 +8,4 @@ class Buy(Resource):
         username =request.values.get('idcard')
         password = request.values.get('cvc')
         print('username: ' + username)
-        self.run()
-        return username
-    def run(self):
-        profile,proxy = get_profile(self.profile),get_proxy(self.proxies)
-        if profile == None:
-            self.status_signal.emit({"msg":"Invalid profile","status":"error"})
-            return
-        if proxy == None:
-            self.status_signal.emit({"msg":"Invalid proxy list","status":"error"})
-            return
-        Walmart(self.task_id,self.status_signal,self.image_signal,self.product,profile,proxy,self.monitor_delay,self.error_delay,self.max_price)
+        #run() Walmart class
